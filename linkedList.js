71331ddeb2ec945d3b2f681b2600cd636b0bb1e5
Stephen <stephenlallum@gmail.com>
2016-03-14 20:05:50 -1000

    got to remove

--- linkedList.js
+++ linkedList.js
@@ -2,7 +2,78 @@
  * @name  linkedListGenerator
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
+ //
  */
-function linkedListGenerator(){
+function linkedListGenerator() {
+
+	var head = null;
+	var tail = null;
+	
+
+            
+    function getHead() {
+    	return head;
+    }
+
+    function getTail() {
+    	return tail;
+    }
+
+    function add(value) {
+    	var node = {
+			value: value,
+			next: null
+		};
+		if (head === null && tail === null){
+		head = node;
+		tail = node;
+	}else {
+		tail.next = node;
+		tail = node;
+	}
+		return node;
+    }
+    
+
+    function get(number) {
+    	var newHead = head;
+    	for (var i = 0; i < number; i++){
+    		if (newHead.next === null){
+    			return false;
+    		}
+    		else {
+    			newHead = newHead.next;
+    		}
+    	}
+    	console.log(newHead);
+    return newHead;
+} 
+
+    	
+
+
+
+    
+
+    function remove(number) {
+
+    }
+
+    function insert(value, number) {
+
+    }
+
+
+
+
+    return {
+
+        getHead: getHead,
+        getTail: getTail,
+        add: add,
+        get: get,
+        remove: remove,
+        insert: insert
+	};   
 
 }
